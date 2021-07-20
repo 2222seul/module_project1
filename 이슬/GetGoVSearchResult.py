ad3f6275c681c157efdd3a239387ae27b202009d
2222seul <87678388+2222seul@users.noreply.github.com>
2021-07-20 14:28:23 +0900

    Update GetGoVSearchResult.py

--- 이슬/GetGoVSearchResult.py
+++ 이슬/GetGoVSearchResult.py
@@ -4,6 +4,7 @@ import urllib.request
 def Get_Request_GoUrl(url):
     pass
 
+
 def GetGoVSearchResult(baseurl, pageValue, perPageValue):   
     paraData = "page=" + str(pageValue)
     paraData += "&perPage=" + str(perPageValue)
@@ -17,3 +18,24 @@ def GetGoVSearchResult(baseurl, pageValue, perPageValue):
         return None
     else:
         return json.loads(resultData)
+    
+    
+def Main():
+    baseurl = ""
+    pageData = 
+    perPageData = 
+
+    jsonSearchResult = GetGoVSearchResult(baseurl, pageData, perPageData)
+
+    with open('%s_GoVData_%s.json' % ('예방접종센터', '데이터'), 'w', encoding='utf-8') as filedata:
+        rJson = json.dumps(jsonSearchResult,
+                            indent = 4, 
+                            sort_keys = True, 
+                            ensure_ascii = False)
+        filedata.write(rJson)
+
+    print('파일이름 : %s_GoVData_%s.json 저장완료' % ('예방접종센터', '데이터'))
+
+    
+if __name__ == '__main__':
+    Main()   
